46497bff1ee471532f547fcb08912033db81d90f
Nora Filep <filepnora@gmail.com>
2019-09-20 17:58:04 +0200

    genre enum init

---
+++ src/main/java/com/codecool/filepnora/remind/model/media/Genre.java
@@ -0,0 +1,17 @@
+package com.codecool.filepnora.remind.model.media;
+
+public enum Genre {
+    CRIME,
+    HORROR,
+    THRILLER,
+    COMEDY,
+    DOCUMENTARY,
+    ACTION,
+    HISTORICAL,
+    SCIFI,
+    FANTASY,
+    ROMANTIC,
+    DRAMA,
+    REALITY,
+    SOAP_OPERA
+}
