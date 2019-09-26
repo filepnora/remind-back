55b0a83ad390c147ad4c456a0e163764d5ae5cb4
Nora Filep <filepnora@gmail.com>
2019-09-26 23:12:10 +0200

    methods for getting data to note page

--- src/main/java/com/codecool/filepnora/remind/controller/NoteController.java
+++ src/main/java/com/codecool/filepnora/remind/controller/NoteController.java
@@ -1,9 +1,35 @@
 package com.codecool.filepnora.remind.controller;
 
+import com.codecool.filepnora.remind.model.note.Note;
+import com.codecool.filepnora.remind.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 public class NoteController {
+
+    @Autowired
+    NoteRepository noteRepository;
+
+    @GetMapping("/notes")
+    private List<Note> getAllNotes(){
+        return noteRepository.getAllNotes();
+    }
+
+    @GetMapping("/notes/{id}")
+    private Note getNoteById(@PathVariable("id") Long noteId){
+        return noteRepository.getById(noteId);
+    }
+
+    @GetMapping("/user/{userId}/notes")
+    private List<Note> getNotesForUser(@PathVariable("userId") Long userId) {
+        return noteRepository.getNotesByUserId(userId);
+    }
 }
--- src/main/java/com/codecool/filepnora/remind/repository/NoteRepository.java
+++ src/main/java/com/codecool/filepnora/remind/repository/NoteRepository.java
@@ -2,6 +2,20 @@ package com.codecool.filepnora.remind.repository;
 
 import com.codecool.filepnora.remind.model.note.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    @Query("SELECT n FROM note n")
+    List<Note> getAllNotes();
+
+    Note getById(Long id);
+
+    @Query(value = "SELECT n FROM note n where user_id = : id", nativeQuery = true)
+    List<Note> getNotesByUserId(Long userId);
+
+
 }
