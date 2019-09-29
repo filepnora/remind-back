4e2b2c445e97cef00ed35f1e067a2b0dd3d402df
Nora Filep <filepnora@gmail.com>
2019-09-29 22:57:33 +0200

    dummy data for init project

--- src/main/java/com/codecool/filepnora/remind/RemindApplication.java
+++ src/main/java/com/codecool/filepnora/remind/RemindApplication.java
@@ -1,7 +1,11 @@
 package com.codecool.filepnora.remind;
 
+import com.codecool.filepnora.remind.model.media.Media;
+import com.codecool.filepnora.remind.model.media.MediaType;
+import com.codecool.filepnora.remind.model.media.Status;
 import com.codecool.filepnora.remind.model.note.Note;
 import com.codecool.filepnora.remind.model.user.User;
+import com.codecool.filepnora.remind.repository.MediaRepository;
 import com.codecool.filepnora.remind.repository.NoteRepository;
 import com.codecool.filepnora.remind.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +25,9 @@ public class RemindApplication {
 
     @Autowired
     NoteRepository noteRepository;
+
+    @Autowired
+    MediaRepository mediaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(RemindApplication.class, args);
@@ -43,6 +50,24 @@ public class RemindApplication {
             userRepository.save(nori);
             noteRepository.save(shopping_list);
             noteRepository.save(vet_appointment);
+
+            Media the_great_gatsby = Media.builder().title("The Great Gatsby")
+                    .releaseDate(1925).status(Status.WANT).type(MediaType.BOOK).author("F. Scott Fitzgerald").user(nori)
+                    .dateAdded(LocalDate.of(2019,9,10)).build();
+
+            Media orwell_book_1984 = Media.builder().title("1984").releaseDate(1949).author("George Orwell").user(nori)
+                    .status(Status.IN_PROGRESS).type(MediaType.BOOK).dateAdded(LocalDate.of(2019,8,10)).build();
+
+            Media monte_cristo = Media.builder().title("The Count of Monte Cristo").releaseDate(1844).user(nori)
+                    .author("Alexandre Dumas").status(Status.WANT).type(MediaType.BOOK).build();
+
+            mediaRepository.save(the_great_gatsby);
+            mediaRepository.save(orwell_book_1984);
+            mediaRepository.save(monte_cristo);
+
+
+
+
         };
     }
 
