8c1917333f0d042e03fe4bb1111ecd8270e0d0d4
Nora Filep <filepnora@gmail.com>
2019-10-07 00:40:35 +0200

    refactor

--- src/main/java/com/codecool/filepnora/remind/controller/NoteController.java
+++ src/main/java/com/codecool/filepnora/remind/controller/NoteController.java
@@ -1,14 +1,15 @@
 package com.codecool.filepnora.remind.controller;
 
 import com.codecool.filepnora.remind.model.note.Note;
+import com.codecool.filepnora.remind.model.user.User;
 import com.codecool.filepnora.remind.repository.NoteRepository;
+import com.codecool.filepnora.remind.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
@@ -19,17 +20,17 @@ public class NoteController {
     NoteRepository noteRepository;
 
     @GetMapping("/notes")
-    private List<Note> getAllNotes(){
+    private List<Note> getAllNotes() {
         return noteRepository.getAllNotes();
     }
 
-    @GetMapping("/notes/{id}")
-    private Note getNoteById(@PathVariable("id") Long noteId){
+    @GetMapping("/notes/{noteId}")
+    private Note getNoteById(@PathVariable Long noteId) {
         return noteRepository.getById(noteId);
     }
 
-    @GetMapping("/user/{userId}/notes")
-    private List<Note> getNotesForUser(@PathVariable("userId") Long userId) {
+    @GetMapping("/users/{userId}/notes")
+    private List<Note> getNotesForUser(@PathVariable Long userId) {
         return noteRepository.getNotesByUserId(userId);
     }
 }
